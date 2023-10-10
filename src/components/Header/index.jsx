3322e9eda5ff2864f695dce27b35273983918c1e
Rabearivony Loïc <122444363+space9cowboy@users.noreply.github.com>
2023-10-10 10:29:40 +0200

    Ajout d la page profils et des card

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,13 +1,13 @@
 import { Link } from 'react-router-dom'
- 
-function Header() {
-    return (
-        <nav>
-            <Link to="/">Accueil</Link>
-            <Link to="/survey/42">Questionnaire</Link>
 
-        </nav>
-    )
+function Header() {
+  return (
+    <nav>
+      <Link to="/">Accueil</Link>
+      <Link to="/survey/1">Questionnaire</Link>
+      <Link to="/freelances">Profils</Link>
+    </nav>
+  )
 }
 
 export default Header
